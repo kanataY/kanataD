78c03c468fdb40dafeecb1b8f11cf2bb95c49e9e
nakano <h1201615016@hiratagakuen.onmicrosoft.com>
2018-04-19 11:09:05 +0900

    指南書１のやつ追加

---
+++ Torch runner/Torch runner/SceneMain.cpp
@@ -0,0 +1,41 @@
+//STLデバック機能をOFFにする
+#define _SECURE_SCL (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+//GameLで使用するヘッダー
+#include "GameL\SceneObjManager.h"
+#include "GameL\DrawTexture.h"
+#include "GameL\DrawFont.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//使用ヘッダー
+#include "SceneMain.h"
+#include "GameHead.h"
+
+
+CSceneMain::CSceneMain()
+{
+
+}
+
+//デストラクタ
+CSceneMain::~CSceneMain()
+{
+
+}
+
+//初期化メソッド
+void CSceneMain::InitScene()
+{
+	 //外部グラフィックファイルを読み込み1番に登録
+	Draw::LoadImageW(L"Image\\bcak.png", 1, TEX_SIZE_512); //オセロ盤
+}
+
+//実行中メソッド
+void CSceneMain::Scene()
+{
+
+}
+
---
+++ Torch runner/Torch runner/main.h
@@ -0,0 +1,19 @@
+#pragma once
+//ウィンドウサイズ
+#define WINDOW_SIZE_W	(512)
+#define WINDOW_SIZE_H	(512)
+//解像度サイズ
+#define PIXEL_SIZE_W	(512)
+#define PIXEL_SIZE_H	(512)
+//一つのシーンで読み込めるグラフィック総数
+#define SCENE_IMG_MAX	(64)
+//一つのシーンで読み込める音楽総数
+#define SCENE_AUDIO_MAX	(32)
+//バックグラウンド色
+#define BACK_COLOR      {0.0f, 0.0f, 0.0f, 1.0f}
+
+//ウィンドウゲームタイトル名
+#define GAME_NAME L"ライブラリ"
+
+
+
