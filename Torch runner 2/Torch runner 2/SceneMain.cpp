121736dc22f442bb30b735c9a8894c632027e94c
nakano <h1201615016@hiratagakuen.onmicrosoft.com>
2018-04-20 14:31:15 +0900

    komitto

---
+++ Torch runner 2/Torch runner 2/SceneMain.cpp
@@ -0,0 +1,40 @@
+//STLデバック機能をOFFにする
+#define _SECURE_SCL (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+//GameLで使用するヘッダー
+#include "GameL\SceneObjManager.h"
+#include "GameL\DrawTexture.h"
+#include "GameL\DrawFont.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//使用ヘッダー
+#include "SceneMain.h"
+#include "GameHead.h"
+
+
+CSceneMain::CSceneMain()
+{
+
+}
+
+//デストラクタ
+CSceneMain::~CSceneMain()
+{
+
+}
+
+//初期化メソッド
+void CSceneMain::InitScene()
+{
+
+}
+
+//実行中メソッド
+void CSceneMain::Scene()
+{
+
+}
+
