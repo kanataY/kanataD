6f80609872a36a43da4ae27d12a2d5a63dc0ee23
kanataD <h1201615004@hiratagakuen.onmicroot.com>
2018-05-18 13:27:24 +0900

    追加

--- Torch runner 2/Torch runner 2/SceneMain.cpp
+++ Torch runner 2/Torch runner 2/SceneMain.cpp
@@ -78,6 +78,8 @@ void CSceneMain::InitScene()
 	//外部グラフィックファイルを読み込み13番に登録(512ピクセル)
 	Draw::LoadImageW(L"Image\\object\\okama_hug.png", 14, TEX_SIZE_512);    //オカマハグ
 
+	//外部グラフィックファイルを読み込み13番に登録(512ピクセル)
+	Draw::LoadImageW(L"Image\\object\\hole.png", 15, TEX_SIZE_64);    //穴
 	//主人公オブジェクト作成
 	CObjRunner* obj = new CObjRunner();
 	Objs::InsertObj(obj, OBJ_RUNNER, 13);
